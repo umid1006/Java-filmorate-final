38a997bb199b79498ee7b9eee6d6a6045582f871
User Umid <umidjoninagamov@yandex.ru>
2024-11-26 19:23:19 +0500

    first commit

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,8 +8,17 @@ import java.time.LocalDate;
 @Data
 public class Film {
     private int id;
+
+    @NotBlank(message = "Название фильма не может быть пустым")
     private String name;
+
+    @Size(max = 200, message = "Максимальная длина описания — 200 символов")
     private String description;
+
+    @NotNull(message = "Дата релиза не может быть null")
+    @PastOrPresent(message = "Дата релиза не может быть в будущем") // Добавьте эту аннотацию
     private LocalDate releaseDate;
-    private int duration; // in minutes
+
+    @Min(value = 1, message = "Продолжительность фильма должна быть положительным числом")
+    private int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,15 +1,16 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
 @Data
 public class User {
-    private int id;
-    private String email;
-    private String login;
-    private String name;
+    int id;
+    String email;
+    String login;
+    String name;
 
     private LocalDate birthday;
 }
